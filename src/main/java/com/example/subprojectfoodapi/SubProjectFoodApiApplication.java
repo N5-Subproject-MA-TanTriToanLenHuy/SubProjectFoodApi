76b53afa1259b672ddb25c4cdf6810bf703c43ef
maihuytech@gmail.com <maihuytech@gmail.com>
2021-11-19 16:02:59 +0700

    init

---
+++ src/main/java/com/example/subprojectfoodapi/SubProjectFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.subprojectfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubProjectFoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubProjectFoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/subprojectfoodapi/SubProjectFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subprojectfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubProjectFoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
