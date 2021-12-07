0d281d54e9592106f3c36f4b8789897cd7aa585b
Phung Thanh Toan <toanpt322@gmail.com>
2021-12-07 10:49:09 +0700

    Toan_Cart

---
+++ src/main/java/com/example/subprojectfoodapi/Entity/Cart.java
@@ -0,0 +1,29 @@
+package com.example.subprojectfoodapi.Entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name="tbl_carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    private String name;
+
+    private double price;
+
+    private String urlImage;
+
+}
